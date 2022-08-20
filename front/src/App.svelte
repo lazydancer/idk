15433a030065add618c4c51a108c611350015e80
Lazydancer <jrpucula@gmail.com>
2022-08-20 10:55:53 -0400

    image-sprites not working

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -97,34 +97,34 @@
 	<html lang="en" />
 </svelte:head>
 
-<header class="flex flex-row flex-nowrap max-w-none mx-auto px-12 py-4 border-b border-gray-300 bg-white sticky top-0">
-  
-  <h1 on:click={() => changeGroup({"g": "All"})} class="flex-1 text-left text-4xl font-extrabold text-red-700 cursor-pointer mr-20 p-1 w-20 flex-none">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 9 5" shape-rendering="crispEdges"><path stroke="#c40424" d="M0 0h1m3 0h1m1 0h1M4 1h1m1 0h1M0 2h1m2 0h2m1 0h1m1 0h1M0 3h1m1 0h1m1 0h1m1 0h2M0 4h1m1 0h3m1 0h1m1 0h1"/></svg>
-  
-  </h1>
-  
-  <form class="items-center flex-1">   
-    <label for="simple-search" class="sr-only">Search</label>
-    <div class="relative w-full">
-        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-        </div>
-        <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required>
+<header class="max-w-none mx-auto px-12 py-4 border-b border-gray-300 bg-white fixed top-0 w-full">
+  <div class="flex flex-row flex-nowrap">
+    <h1 on:click={() => changeGroup({"g": "All"})} class="flex-1 text-left text-4xl font-extrabold text-red-700 cursor-pointer mr-20 p-1 w-20 flex-none">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 9 5" shape-rendering="crispEdges"><path stroke="#c40424" d="M0 0h1m3 0h1m1 0h1M4 1h1m1 0h1M0 2h1m2 0h2m1 0h1m1 0h1M0 3h1m1 0h1m1 0h1m1 0h2M0 4h1m1 0h3m1 0h1m1 0h1"/></svg>
+    
+    </h1>
+    
+    <form class="items-center flex-1">   
+      <label for="simple-search" class="sr-only">Search</label>
+      <div class="relative w-full">
+          <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+          </div>
+          <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required>
+      </div>
+    </form>
+
+    <div class="mx-5 mt-3 flex-1">
+      {#if displayOrder}
+        <span class="text-sm text-gray-900 cursor-pointer font-bold float-right">Order {orderLength}</span>
+      {:else}
+        <span on:click={()=>setOrder()} class="text-sm text-gray-900 cursor-pointer float-right">Order {orderLength}</span>
+      {/if}
     </div>
-  </form>
-
-  <div class="mx-5 mt-3 flex-1">
-    {#if displayOrder}
-      <span class="text-sm text-gray-900 cursor-pointer font-bold float-right">Order {orderLength}</span>
-    {:else}
-      <span on:click={()=>setOrder()} class="text-sm text-gray-900 cursor-pointer float-right">Order {orderLength}</span>
-    {/if}
   </div>
-
 </header>
 
-<main class="flex flex-row items-start px-12 py-6 max-w-none mx-auto">
+<main class="flex flex-row items-start px-12 py-6 max-w-none mx-auto mt-20">
   <ul class="text-gray-900 text-sm visited:text-gray-900 pr-10 h-screen fixed top-30 w-40">
     <div class="h-3"></div>
     {#each groups as g}
@@ -142,9 +142,9 @@
     <Accordion>
       {#each visibleItems as item}
         <AccordionItem key={item.key}>
-            <div slot='header' class="flex flex-row py-2">
+            <div slot='header' class="flex flex-row py-1">
               <div class="mr-4" style="width: 37px; height: 37px; background: #8B8B8B; border: 2px solid; border-color: #373737 #FFF #FFF #373737">
-                <div style="width:32px; height:32px; background-image:url(static/InvSprite.webp); background-position: {item.imageLoc[0]}px {item.imageLoc[1]}px"></div>
+                <div style="width:32px; height:32px; background-image:url(static/items-sprite.png); background-position: {item.imageLoc[0]}px {item.imageLoc[1]}px"></div>
               </div>
               <h2 class="flex-1 w-72 pt-1">{ item.display_name }</h2>
               <p class="flex-1 text-sm pt-1.5 pr-2">{item.count}</p>
---
+++ front/tools/extract_image_sprite/run.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+from requests import get
+import tinycss
+import re
+import json
+
+base_url = "https://minecraft-ids.grahamedgecombe.com/"
+base_url_html = get(base_url).text
+css_parser = tinycss.make_parser('page3')
+
+soup = BeautifulSoup(base_url_html, "html.parser")
+items = soup.find_all("tr", {"class": "row"})
+stylesheets = [base_url + style["href"] for style in soup.select('link[rel="stylesheet"]')]
+
+sprite_sheet_url = None
+positions = {}
+li = {}
+
+
+for sheet in stylesheets:
+    data = get(sheet).text
+    style = css_parser.parse_stylesheet(data)
+
+    for rule in style.rules:
+        selector = rule.selector.as_css()  # selector name (i.e. .items-*-*-*)
+
+        if re.search(r'.items-', selector[:7]):
+            [url, x, y, _] = rule.declarations[2].value.as_css().split(" ")
+
+            positions[selector[7:]] = (
+                abs(int(x.replace("px", ""))),
+                abs(int(y.replace("px", "")))
+            )
+
+            if (sprite_sheet_url == None):
+                sprite_sheet_url = base_url + url[4:-1]
+
+
+
+result = {}
+for a in items:
+    pos_id = a.find("td", {"class": "row-icon"}).div["class"][1][6:]
+    name_id = a.find("td", {"class": "row-desc"}).find("span", {"class": "text-id"}).text.split(":")[1][:-1]
+    pos = positions[pos_id]
+
+    result[name_id] = pos
+
+with open('spirte_map.json', 'w') as f:
+    json.dump(result, f)
