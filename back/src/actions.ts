eddf313b43cfd736091f2bec32778204901cd60a
Lazydancer <jrpucula@gmail.com>
2022-08-21 23:47:53 -0400

    moves action added

--- back/src/actions.ts
+++ back/src/actions.ts
@@ -66,54 +66,58 @@ export async function withdraw(items: any, station: number) {
 
 }
 
-{
-    from: ["inventory", 4, 2],
-    to: ["station", 2, 1],
-    amount: 4 
-}
+// {
+//     from: {
+//         chest_type: "inventory", 
+//         chest: 4,
+//         slot: 2,
+//     }
+//     to: {
+//         chest_type: "station", 
+//         chest: 2,
+//         slot: 1,   
+//     }
+//     count: 4 
+// }
 
 export async function move(requests: any) {
     const chunkSize = 36 // inventory
 
-    for (let i = 0; i < items.length; i += chunkSize) {
-        const chunk = items.slice(i, i + chunkSize);
+    for (let a = 0; a < requests.length; a += chunkSize) {
+        const chunk = requests.slice(a, a + chunkSize);
 
-        let open_slot = 0
         let prev_chest = null;
 
-        let c: any
-        for(c of chunk) {
+        let c, i: any
+
+        for([i, c] of chunk.entries()) {
 
             let chest: any;
-            if ((prev_chest === null) || (prev_chest !== c.chest)) {
-                chest = await global.player.open("inventory", c.chest)
-                prev_chest = c.chest
+            if ((prev_chest === null) || (prev_chest !== c.from.chest)) {
+                chest = await global.player.open(c.from.chest_type, c.from.chest)
+                prev_chest = c.from.chest
             }
 
-            await global.player.lclick(c.slot)
+            await global.player.lclick(c.from.slot)
 
-
-            // place and return extra
-            if (c.count == c.available) {
-                await global.player.lclick(open_slot + 54)
-            } else if (c.count < c.available) {
-                for (let _a = 0; _a < c.count; _a += 1) {
-                    await global.player.rclick(open_slot + 54)
-                }
-                await global.player.lclick(c.slot)
-            } else {
-                console.error('asking for more in slot than avaiable')
+            for (let _a = 0; _a < c.count; _a += 1) {
+                await global.player.rclick(i + 54)
             }
-
-            open_slot += 1
-
+            await global.player.lclick(c.from.slot)
+            
         }
 
-        await global.player.open("station", station)
+        for([i, c] of chunk.entires()) {
 
-        for (let i = 0; i < open_slot; i++) {
+            let chest: any;
+            if ((prev_chest === null) || (prev_chest !== c.to.chest)) {
+                chest = await global.player.open(c.to.chest_type, c.to.chest)
+                prev_chest = c.to.chest
+            }
+
             await global.player.lclick(i + 54)
-            await global.player.lclick(i)
+            await global.player.lclick(c.to.slot)
+            
         }
 
     }
--- back/src/inventory.ts
+++ back/src/inventory.ts
@@ -21,6 +21,19 @@ export async function withdraw(items: any, station: number) {
 
 }
 
+export async function deposit(station: number) {
+    let items = await actions.get_chest_contents(station)
+
+    const inventory = await getItems();
+
+    const items_w_moves = find_spaces(items, inventory)
+
+    // await actions.deposit(items_w_moves);
+
+}
+
+
+
 function find(items: any, inventory: any) {
     let result = [];
     
@@ -54,12 +67,64 @@ function find(items: any, inventory: any) {
 
 }
 
+function find_spaces(items: any, inventory: any) {
+    let result = [];
+
+    let item, i: any
+    for ([i, item] of items.entries()) {
+        let inv_item: any
+        for (inv_item of inventory) {
+            
+            if ( !matches(item, inv_item) ){
+                continue;
+            }
+            if (inv_item.count == inv_item.stack_size){
+                continue;
+            }
+
+            let space_in_stack = inv_item.stack_size - inv_item.count
+            
+            if (space_in_stack >= item.count) {
+                item['to'] = ["inventory", inv_item.chest, inv_item.slot]
+                
+                inv_item.count += (item.count - space_in_stack)
+
+                result.push(item)
+                break;
+            }
+
+        }
+
+        if(! ("to" in item)) {
+            item['to'] = next_open_slot(inventory)
+
+            inventory.push({
+                'name': 'hold',
+                'chest': item['to'][1],
+                'slot': item['to'][2],
+            })
+
+            result.push(item)
+        }        
+    }
+
+    return result
+
+}
+
+function next_open_slot(inventory: any) {
+    let counts = global.player.get_counts()
+    for(let i=0; i<counts["inventory"]; i++) {
+        for(let j=0; j<54; j++) {
+            if(!inventory.some((e: any) => (e.chest == i) && (e.slot == j))){
+                return ["inventory", i, j]
+            }
+        }
+    }
+}
+
 function matches(item: any, other: any): boolean {
     return (item.name === other.name)  
     && (item.metadata === other.metadata)
     && (item.nbt === other.nbt)
-}
-
-async function deposit(station: number) {
-    
 }
